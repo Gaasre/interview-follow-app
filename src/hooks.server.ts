0123221e1fb10cab2f99951704faed1031ca10ad
Gaasre <amine.barchid@gmail.com>
2023-10-18 19:37:12 +0200

    add route protection

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,32 +1,43 @@
 import { getSelf } from '$api/user';
 import type { User } from '$types/types';
-import type { Handle } from '@sveltejs/kit';
+import { redirect, type Handle } from '@sveltejs/kit';
 import { sessionToken } from './stores';
+
+const PUBLIC_ROUTES = ['/signin', '/signup', '/'];
+const AUTH_ROUTES = ['/signin', '/signup'];
 
 export const handle: Handle = async ({ event, resolve }) => {
 	const token = event.cookies.get('token');
 
 	if (token) {
 		sessionToken.set(token);
-		try {
-			const user = await getSelf();
+		const user = await getSelf();
 
-			if (!user) {
-				throw new Error('User not found');
+		const requestedPath = event.url.pathname;
+
+		if (!user) {
+			if (!PUBLIC_ROUTES.some((value) => requestedPath.startsWith(value))) {
+				// not a public route and we don't have a user
+				// redirect to signin
+				throw redirect(303, '/signin');
 			}
-
-			const sessionUser: User = {
-				id: user.id,
-				email: user.email,
-				name: user.name
-			};
-
-			console.log(sessionUser);
-
-			event.locals.user = sessionUser;
-		} catch (error) {
-			console.error(error);
 		}
+
+		if (AUTH_ROUTES.some((value) => requestedPath.startsWith(value))) {
+			// auth route and we already have a user
+			// redirect to the dashboard
+			throw redirect(303, '/dashboard');
+		}
+
+		const sessionUser: User = {
+			id: user.id,
+			email: user.email,
+			name: user.name
+		};
+
+		console.log('session', sessionUser);
+
+		event.locals.user = sessionUser;
 	}
 
 	return await resolve(event);
