d10a4782ce9f024d798ae592179b30eaaeaa98ee
Gaasre <amine.barchid@gmail.com>
2023-10-19 03:49:46 +0200

    dynamic sidebar

--- src/routes/dashboard/(components)/sidebar.svelte
+++ src/routes/dashboard/(components)/sidebar.svelte
@@ -1,6 +1,9 @@
-<script>
+<script lang="ts">
 	import { Button } from '$lib/components/ui/button';
 	import { Briefcase, Calendar, LayoutDashboard, ListTodo, UserCircle } from 'lucide-svelte';
+	import { page } from '$app/stores';
+
+	const variant = (page: string) => ($page.url.pathname.includes(page) ? 'secondary' : 'ghost');
 </script>
 
 <div class="pb-12 min-h-screen h-full">
@@ -8,19 +11,19 @@
 		<div class="px-4 py-2">
 			<h2 class="mb-2 px-4 text-lg font-semibold tracking-tight">Menu</h2>
 			<div class="space-y-1">
-				<Button class="w-full justify-start space-x-4" variant="secondary">
+				<Button href="/dashboard" class="w-full justify-start space-x-4" variant={variant('dashboard')}>
 					<LayoutDashboard size={16} />
 					<span>Dashboard</span>
 				</Button>
-				<Button class="w-full justify-start space-x-4" variant="ghost">
+				<Button href="/calendar" class="w-full justify-start space-x-4" variant={variant('calendar')}>
 					<Calendar size={16} />
 					<span>Calendar</span>
 				</Button>
-				<Button class="w-full justify-start space-x-4" variant="ghost">
+				<Button href="/applications" class="w-full justify-start space-x-4" variant={variant('applications')}>
 					<Briefcase size={16} />
 					<span>Job Applications</span>
 				</Button>
-				<Button class="w-full justify-start space-x-4" variant="ghost">
+				<Button href="/interviews" class="w-full justify-start space-x-4" variant={variant('interviews')}>
 					<ListTodo size={16} />
 					<span>Interviews</span>
 				</Button>
@@ -29,7 +32,7 @@
 		<div class="px-4 py-2">
 			<h2 class="mb-2 px-4 text-lg font-semibold tracking-tight">Settings</h2>
 			<div class="space-y-1">
-				<Button class="w-full justify-start space-x-4" variant="secondary">
+				<Button href="/account" class="w-full justify-start space-x-4" variant={variant('account')}>
 					<UserCircle size={16} />
 					<span>Account</span>
 				</Button>
